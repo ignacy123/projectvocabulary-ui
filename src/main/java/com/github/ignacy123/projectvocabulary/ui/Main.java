97c819b615bb1ecc2d155ab7fea4e1f633a7563b
ignacy123 <ignacy.buczek@onet.pl>
2016-01-14 16:05:10 +0100

    Created initial javafx application

---
+++ src/main/java/com/github/ignacy123/projectvocabulary/ui/Main.java
@@ -0,0 +1,26 @@
+package com.github.ignacy123.projectvocabulary.ui;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Created by ignacy on 14.01.16.
+ */
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
+        primaryStage.setTitle("Project vocabulary");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+
+    }
+}
