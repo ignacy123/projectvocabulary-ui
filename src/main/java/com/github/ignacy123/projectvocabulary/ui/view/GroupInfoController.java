10230b1753e892d3ef9f96b5cd181b13665e0832
ignacy <ignacy.buczek@onet.pl>
2016-10-19 16:35:05 +0200

    Fixed groupInfo scene

--- src/main/java/com/github/ignacy123/projectvocabulary/ui/view/GroupInfoController.java
+++ src/main/java/com/github/ignacy123/projectvocabulary/ui/view/GroupInfoController.java
@@ -2,20 +2,38 @@ package com.github.ignacy123.projectvocabulary.ui.view;
 
 import com.github.ignacy123.projectvocabulary.ui.Main;
 import com.github.ignacy123.projectvocabulary.ui.domain.User;
-import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+import java.util.List;
 
 /**
  * Created by ignacy on 11.10.16.
  */
-public class GroupInfoController extends  AbstractBaseController{
+public class GroupInfoController extends AbstractBaseController {
     @FXML
     private Label name;
     @FXML
     private Label amountOfStudents;
-     public void init (Main main){
-         name.setText(main.getCurrentGroup().getName());
-         amountOfStudents.setText(String.valueOf(main.getCurrentGroup().getStudents().size()));
-     }
+    @FXML
+    private ListView shownStudents;
+
+    private ObservableList<User> students = FXCollections.observableArrayList();
+
+    public void init(Main main) {
+        super.init(main);
+        name.setText(main.getCurrentGroup().getName());
+        amountOfStudents.setText(String.valueOf(main.getCurrentGroup().getStudents().size()));
+        for (User user : main.getCurrentGroup().getStudents()) {
+            students.add(user);
+        }
+        shownStudents.setItems(students);
+    }
+
+    public void back() {
+        main.switchToGroupsScene();
+    }
 }
