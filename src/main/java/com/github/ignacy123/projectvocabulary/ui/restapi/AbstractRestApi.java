7b2407e045c591220a5ed7037138fcd078390cd5
Michal Poroslo <spojowa>
2016-11-09 16:33:35 +0100

    Added Logging request interceptor

--- src/main/java/com/github/ignacy123/projectvocabulary/ui/restapi/AbstractRestApi.java
+++ src/main/java/com/github/ignacy123/projectvocabulary/ui/restapi/AbstractRestApi.java
@@ -16,79 +16,80 @@ import java.io.IOException;
  * Created by ignacy on 14.09.16.
  */
 public abstract class AbstractRestApi {
-    protected final RestTemplate restTemplate;
-    protected final ObjectMapper mapper;
+	protected final RestTemplate restTemplate;
+	protected final ObjectMapper mapper;
 
-    protected AbstractRestApi() {
-        restTemplate = new RestTemplate();
-        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
-        mapper = new ObjectMapper();
-    }
+	protected AbstractRestApi() {
+		restTemplate = new RestTemplate();
+		restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+		restTemplate.getInterceptors().add(new LoggingRequestInterceptor());
+		mapper = new ObjectMapper();
+	}
 
-    protected <T> ResponseEntity<T> post(String url, Object object, Class<T> returnClass) {
-        try {
-            ResponseEntity<T> resultResponseEntity = restTemplate.postForEntity(
-                    "http://localhost:8080/projectvocabulary" + url,
-                    object,
-                    returnClass);
-            return resultResponseEntity;
-        } catch (HttpClientErrorException e) {
-            String errorJson = e.getResponseBodyAsString();
-            try {
-                ErrorDto errorDto = mapper.readValue(errorJson, ErrorDto.class);
-                throw new RestValidationException(errorDto);
-            } catch (IOException e1) {
-                throw new RuntimeException(e1);
-            }
-        }
+	protected <T> ResponseEntity<T> post(String url, Object object, Class<T> returnClass) {
+		try {
+			ResponseEntity<T> resultResponseEntity = restTemplate.postForEntity(
+					"http://localhost:8080/projectvocabulary" + url,
+					object,
+					returnClass);
+			return resultResponseEntity;
+		} catch (HttpClientErrorException e) {
+			String errorJson = e.getResponseBodyAsString();
+			try {
+				ErrorDto errorDto = mapper.readValue(errorJson, ErrorDto.class);
+				throw new RestValidationException(errorDto);
+			} catch (IOException e1) {
+				throw new RuntimeException(e1);
+			}
+		}
 
-    }
+	}
 
-    protected <T> ResponseEntity<T> postWithCookie(String url, Object object, Class<T> returnClass, String cookie) {
-        return sendWithCookie(HttpMethod.POST, url, object, returnClass, cookie);
+	protected <T> ResponseEntity<T> postWithCookie(String url, Object object, Class<T> returnClass, String cookie) {
+		return sendWithCookie(HttpMethod.POST, url, object, returnClass, cookie);
 
-    }
+	}
 
-    protected <T> ResponseEntity<T> putWithCookie(String url, Object object, Class<T> returnClass, String cookie) {
-        return sendWithCookie(HttpMethod.PUT, url, object, returnClass, cookie);
-    }
+	protected <T> ResponseEntity<T> putWithCookie(String url, Object object, Class<T> returnClass, String cookie) {
+		return sendWithCookie(HttpMethod.PUT, url, object, returnClass, cookie);
+	}
 
-    private <T> ResponseEntity<T> sendWithCookie(HttpMethod method, String url, Object object, Class<T> returnClass, String cookie) {
-        try {
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Cookie", cookie);
-            HttpEntity<Object> httpEntity = new HttpEntity<>(object, headers);
-            ResponseEntity<T> responseEntity = restTemplate.exchange("http://localhost:8080/projectvocabulary"+url, method, httpEntity, returnClass);
-            return responseEntity;
-        } catch (HttpClientErrorException e) {
-            String errorJson = e.getResponseBodyAsString();
-            try {
-                ErrorDto errorDto = mapper.readValue(errorJson, ErrorDto.class);
-                throw new RestValidationException(errorDto);
-            } catch (IOException e1) {
-                throw new RuntimeException(e1);
-            }
-        }
+	private <T> ResponseEntity<T> sendWithCookie(HttpMethod method, String url, Object object, Class<T> returnClass, String cookie) {
+		try {
+			HttpHeaders headers = new HttpHeaders();
+			headers.add("Cookie", cookie);
+			HttpEntity<Object> httpEntity = new HttpEntity<>(object, headers);
+			ResponseEntity<T> responseEntity = restTemplate.exchange("http://localhost:8080/projectvocabulary" + url, method, httpEntity, returnClass);
+			return responseEntity;
+		} catch (HttpClientErrorException e) {
+			String errorJson = e.getResponseBodyAsString();
+			try {
+				ErrorDto errorDto = mapper.readValue(errorJson, ErrorDto.class);
+				throw new RestValidationException(errorDto);
+			} catch (IOException e1) {
+				throw new RuntimeException(e1);
+			}
+		}
 
-    }
+	}
 
-    protected  <T> ResponseEntity<T> getWithCookie(String url, Class<T> returnClass, String cookie) {
-        try {
-            HttpHeaders headers = new HttpHeaders();
-            headers.add("Cookie", cookie);
-            HttpEntity<Object> httpEntity = new HttpEntity<>(headers);
-            ResponseEntity<T> responseEntity = restTemplate.exchange("http://localhost:8080/projectvocabulary"+url, HttpMethod.GET, httpEntity, returnClass);
-            return responseEntity;
-        } catch (HttpClientErrorException e) {
-            String errorJson = e.getResponseBodyAsString();
-            try {
-                ErrorDto errorDto = mapper.readValue(errorJson, ErrorDto.class);
-                throw new RestValidationException(errorDto);
-            } catch (IOException e1) {
-                throw new RuntimeException(e1);
-            }
-        }
+	protected <T> ResponseEntity<T> getWithCookie(String url, Class<T> returnClass, String cookie) {
+		try {
+			HttpHeaders headers = new HttpHeaders();
+			headers.add("Cookie", cookie);
+			HttpEntity<Object> httpEntity = new HttpEntity<>(headers);
+			ResponseEntity<T> responseEntity = restTemplate.exchange("http://localhost:8080/projectvocabulary" + url, HttpMethod.GET, httpEntity, returnClass);
+			return responseEntity;
+		} catch (HttpClientErrorException e) {
+			String errorJson = e.getResponseBodyAsString();
+			try {
+				ErrorDto errorDto = mapper.readValue(errorJson, ErrorDto.class);
+				throw new RestValidationException(errorDto);
+			} catch (IOException e1) {
+				throw new RuntimeException(e1);
+			}
+		}
 
-    }
+	}
 
 }
---
+++ src/main/java/com/github/ignacy123/projectvocabulary/ui/restapi/LoggingRequestInterceptor.java
@@ -0,0 +1,46 @@
+package com.github.ignacy123.projectvocabulary.ui.restapi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.client.ClientHttpRequestExecution;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+
+public class LoggingRequestInterceptor implements ClientHttpRequestInterceptor {
+
+	final static Logger LOGGER = LoggerFactory.getLogger(LoggingRequestInterceptor.class);
+
+	@Override
+	public ClientHttpResponse intercept(HttpRequest request, byte[] body, ClientHttpRequestExecution execution) throws IOException {
+		traceRequest(request, body);
+
+		ClientHttpResponse response = execution.execute(request, body);
+		BufferingClientHttpResponseWrapper responseWrapper = new BufferingClientHttpResponseWrapper(response);
+		traceResponse(responseWrapper);
+		return responseWrapper;
+	}
+
+	private void traceRequest(HttpRequest request, byte[] body) throws IOException {
+		LOGGER.debug("============================request begin===========================================");
+		LOGGER.debug("URI         : {}", request.getURI());
+		LOGGER.debug("Method      : {}", request.getMethod());
+		LOGGER.debug("Headers     : {}", request.getHeaders());
+		LOGGER.debug("Request body: {}", new String(body, "UTF-8"));
+		LOGGER.debug("============================request end=============================================");
+	}
+
+	private void traceResponse(ClientHttpResponse response) throws IOException {
+		byte[] responseBytes = StreamUtils.copyToByteArray(response.getBody());
+
+		LOGGER.debug("============================response begin==========================================");
+		LOGGER.debug("Status text  : {}", response.getStatusText());
+		LOGGER.debug("Headers      : {}", response.getHeaders());
+		LOGGER.debug("Response body: {}", new String(responseBytes, "UTF-8"));
+		LOGGER.debug("============================response end============================================");
+	}
+
+}
