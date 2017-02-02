540215055d40cdfe864e32f7e52032f525473d7f
tomkru <tomasz.kruzel@msleopard.com>
2017-02-02 19:23:17 +0100

    missing dependency fix

--- src/main/java/com/github/ignacy123/projectvocabulary/ui/domain/User.java
+++ src/main/java/com/github/ignacy123/projectvocabulary/ui/domain/User.java
@@ -1,107 +1,113 @@
 package com.github.ignacy123.projectvocabulary.ui.domain;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Collection;
 import java.util.Formatter;
-import java.util.Objects;
 
 /**
  * Created by ignacy on 29.10.15.
  */
 public class User implements Serializable {
-    private static final long serialVersionUID = 1L;
 
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private String password;
-    private String email;
-    private String cookie;
-    private Collection<GrantedAuthority> authorities;
+	private static final long serialVersionUID = 1L;
 
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String email;
+	private String cookie;
+	private Collection<GrantedAuthority> authorities;
 
-    public Collection<GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
+	public Collection<GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
 
-    public void setAuthorities(Collection<GrantedAuthority> authorities) {
-        this.authorities = authorities;
-    }
+	public void setAuthorities(Collection<GrantedAuthority> authorities) {
+		this.authorities = authorities;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Long getId() {
+		return id;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getFirstName() { return firstName; }
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public void setFirstName(String firstName) { this.firstName = firstName; }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-    public String getLastName() { return lastName; }
+	public String getLastName() {
+		return lastName;
+	}
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = computeHash(password);
-    }
+	public void setPassword(String password) {
+		this.password = computeHash(password);
+	}
 
-    public boolean passwordMatches(String anotherPassword) {
-        return computeHash(anotherPassword).equals(password);
-    }
+	public boolean passwordMatches(String anotherPassword) {
+		return computeHash(anotherPassword).equals(password);
+	}
 
-    public String getCookie() {
-        return cookie;
-    }
+	public String getCookie() {
+		return cookie;
+	}
 
-    public void setCookie(String cookie) {
-        this.cookie = cookie;
-    }
+	public void setCookie(String cookie) {
+		this.cookie = cookie;
+	}
 
-    public static String computeHash(String str) {
-        String sha1 = "";
-        try {
-            MessageDigest crypt = MessageDigest.getInstance("SHA-1");
-            crypt.reset();
-            crypt.update(str.getBytes("UTF-8"));
-            sha1 = byteToHex(crypt.digest());
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return sha1;
-    }
+	public static String computeHash(String str) {
+		String sha1 = "";
+		try {
+			MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+			crypt.reset();
+			crypt.update(str.getBytes("UTF-8"));
+			sha1 = byteToHex(crypt.digest());
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return sha1;
+	}
 
-    private static String byteToHex(byte[] hash) {
-        Formatter formatter = new Formatter();
-        for (byte b : hash) {
-            formatter.format("%02x", b);
-        }
-        String result = formatter.toString();
-        formatter.close();
-        return result;
+	private static String byteToHex(byte[] hash) {
+		Formatter formatter = new Formatter();
+		for (byte b : hash) {
+			formatter.format("%02x", b);
+		}
+		String result = formatter.toString();
+		formatter.close();
+		return result;
 
-    }
-
+	}
 
 }
